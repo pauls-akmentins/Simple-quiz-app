7b80f7c0b5d9f603daf1ad04f71ce0c57336833e
pauls-akmentins <paulsakmentins@gmail.com>
2023-07-09 14:36:15 +0300

    Move fetching logic to Quiz component

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,16 @@
-import { Box, CircularProgress } from '@mui/material';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { useRoutes } from 'react-router-dom';
 
-import { MockApi } from './apis';
 import { MainContainer } from './layout';
 import { ThemeProvider } from './providers';
 import { routesConfig } from './routesConfig';
-import { useQuestionsStore } from './store';
 
 const App = () => {
   const routes = useRoutes(routesConfig);
-  const { setQuestions } = useQuestionsStore();
-
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    const fetchMockData = async () => {
-      setIsLoading(true);
-      const {
-        data: { questions }
-      } = await MockApi();
-      setIsLoading(false);
-
-      setQuestions(questions);
-    };
-
-    fetchMockData();
-  }, []);
 
   return (
     <ThemeProvider>
-      <MainContainer>
-        {isLoading ? (
-          <Box
-            sx={{
-              height: '100vh',
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-
-              margin: '0 100px'
-            }}>
-            <CircularProgress size={100} color="primary" />
-          </Box>
-        ) : (
-          routes
-        )}
-      </MainContainer>
+      <MainContainer>{routes}</MainContainer>
     </ThemeProvider>
   );
 };
--- src/views/Quiz/Quiz.tsx
+++ src/views/Quiz/Quiz.tsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Box } from '@mui/material';
 import { Navigate, useParams } from 'react-router-dom';
 
 import { useQuestionsStore } from '../../store';
 import { Question } from '../Question';
 import { Nav } from '../../components';
+import { MockApi } from '../../apis';
 
 export const Quiz = () => {
   const { questions } = useQuestionsStore();
   const { questionId } = useParams();
   const numQuestionId = Number(questionId || 0);
+
+  const { setQuestions } = useQuestionsStore();
+
+  useEffect(() => {
+    const fetchMockData = async () => {
+      const {
+        data: { questions }
+      } = await MockApi();
+
+      setQuestions(questions);
+    };
+
+    fetchMockData();
+  }, []);
 
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'space-between' }}>
